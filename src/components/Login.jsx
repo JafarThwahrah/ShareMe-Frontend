c5f9c6ccae630288ad2fc60fa40aeed274066b68
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-03-31 05:48:10 +0300

    login via google and create sanity doc

---
+++ src/components/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+import jwt_decode from "jwt-decode";
+import { client } from "../client";
+function Login() {
+  const navigate = useNavigate();
+  const handleLoginGoogle = (res) => {
+    let decoded = jwt_decode(res.credential);
+    const { name, aud, picture } = decoded;
+    const doc = {
+      _id: aud,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleOAuthProvider
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              buttonText="Login with Google"
+              Referrer-Policy={"no-referrer-when-downgrade"}
+              cookiePolicy={"single_host_origin"}
+            >
+              <GoogleLogin
+                buttonText="Sign in with Google"
+                onSuccess={handleLoginGoogle}
+                onError={handleLoginGoogle}
+                useOneTap
+              />
+            </GoogleOAuthProvider>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
